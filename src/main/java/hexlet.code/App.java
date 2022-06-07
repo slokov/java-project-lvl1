3b5ca3b6d9af2cf4d7bffb9037845e42faac125d
Pavel Mironov <pavel.mironov@sbermarket.ru>
2022-06-07 21:03:46 +0300

    fix

--- src/main/java/hexlet.code/App.java
+++ src/main/java/hexlet.code/App.java
@@ -18,12 +18,12 @@ public class App {
     public static void main(String[] args) {
 
 
-        System.out.print("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc");
-        System.out.println("\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
+        System.out.print("Please enter the game number and press Enter. \n 1 - Greet \n 2 - Even \n 3 - Calc");
+        System.out.println(" \n 4 - GCD \n 5 - Progression \n 6 - Prime \n 0 - Exit");
         Scanner sc = new Scanner(System.in);
         int numberGames = sc.nextInt();
 
-        System.out.println("Your choice: " + numberGames + "\n");
+        System.out.println(" Your choice: " + numberGames + "\n");
         Cli wc = new Cli();
         String userName;
 
